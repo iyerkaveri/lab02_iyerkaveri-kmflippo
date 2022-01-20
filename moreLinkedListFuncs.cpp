fb676940c8688cd4e5665f670faf1bebd6ad221f
Kelly Flippo <kmflippo@cs.ucsb.edu>
2022-01-20 13:46:58 -0800

    largest Value updated

--- moreLinkedListFuncs.cpp
+++ moreLinkedListFuncs.cpp
@@ -113,22 +113,15 @@ Node * pointerToMin(LinkedList *list) {
 
 int largestValue(LinkedList *list) {
 
-  // Code may assume that these assertions are true;
-  //  so does not need to do error checking for these conditions.
-
   assert(list!=NULL);
   assert(list->head != NULL);
 
-  // TODO: Insert code here to calculate and return
-  //   largest value in list (which may not be unique).
-
-  return -42; // STUB!  Replace this line with correct code
-
+  return pointerToMax(list)->data;
 }
 
 // list: ptr to a linked list of Node (each with int data, and Node * next)
 // Return the smallest value in the list.
-// This value may be unique, or may occur more than once
+// This value may be unique, ory occur more than once
 // You may assume list has at least one element
 
 int smallestValue(LinkedList *list) {
